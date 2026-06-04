f23d441d5b00ece8606052b267a5950616d165b3
leechi48 <chinghongmarcus.lee@mail.utoronto.ca>
2026-06-03 21:04:30 -0400

    added collaborator

--- src/teamData.js
+++ src/teamData.js
@@ -5,5 +5,12 @@ export const TEAM_MEMBERS = [
     role: "Founder",
     description: "Sabrina is a recent law graduate from the Chinese University of Hong Kong (PCLL & LLB) and King’s College London (LLM) integrated master’s programme specializing in transnational law. She has a keen interest in climate change litigation and has been engaging in environmental, social, and governance (ESG) work since her undergraduate years. Sabrina's research findings focused on the emerging trends in the ASEAN region and environmental litigation of Belt and Road Initiative participating states in an international conference, incorporated valuable experiences through internships at law firms, NGOs and businesses, and is passionate to further explore issues at the intersections of legal, commercial and ESG policy. Apart from professional and academic work, she manages the policy team of a local youth-led environmental organization (NESS), centering on building skill sets and raising students’ awareness of the emerging green job market.",
     photo: "teamphotos/Sabrina.jpeg",
+  },
+  {
+    id: 2,
+    name: "Marvis Chan",
+    role: "Collaborator",
+    description: "Marvis is a penultimate-year student at the University of Hong Kong. Alongside his studies, he has dedicated time to researching issues in animal and environmental law at both local and global levels. His most recent work examines how the remit of the Agriculture, Fisheries and Conservation Department has shifted over the past decades and how those changes have influenced the allocation of resources. Beyond his academic pursuits, Marvis is always open to thoughtful conversation, whether about his work or his personal interests.",
+    photo: "teamphotos/Marvis.jpeg",
   }
-];
+]; 
