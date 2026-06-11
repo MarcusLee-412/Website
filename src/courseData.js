567ca585498fdbcacf4090d8637da6aa985eff6d
leechi48 <chinghongmarcus.lee@mail.utoronto.ca>
2026-06-11 11:04:05 -0400

    pre-presentation edits

--- src/courseData.js
+++ src/courseData.js
@@ -1,9 +1,9 @@
 export const COURSE_MODULES = [
   {
     id: "m1",
-    title: "Introduction to Microeconomics",
-    description: "Master foundational core principles.",
-    pdfUrl: "documents/test_file_1.pdf", 
+    title: "Chapter 1: Laws of Climate Change",
+    description: "From Local to Global: Navigating Climate Change from a Legal Perspective",
+    pdfUrl: "documents/Chapter 2 Legal Perspective on Climate Change.pdf", 
     csvUrl: "questions/test_questions_1.csv",
   }
 ];
